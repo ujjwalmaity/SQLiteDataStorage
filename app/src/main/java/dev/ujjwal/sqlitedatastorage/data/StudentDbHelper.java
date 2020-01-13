500a7af3bf5c99536597dd0a3800a304441e9d42
ujjwal <ujjwalmaity98@gmail.com>
2020-01-13 12:12:35 +0530

    Changes query structure

--- app/src/main/java/dev/ujjwal/sqlitedatastorage/data/StudentDbHelper.java
+++ app/src/main/java/dev/ujjwal/sqlitedatastorage/data/StudentDbHelper.java
@@ -13,34 +13,41 @@ public class StudentDbHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "studentAttendance.db";
     private static final int DATABASE_VERSION = 1;
 
+    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + StudentEntry.TABLE_NAME + " ("
+            + StudentEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + StudentEntry.COLUMN_QR_CODE_ID + " TEXT NOT NULL, "
+            + StudentEntry.COLUMN_NAME + " TEXT NOT NULL, "
+            + StudentEntry.COLUMN_BATCH + " TEXT NOT NULL);";
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + StudentEntry.TABLE_NAME;
+
     public StudentDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String SQL_CREATE_STUDENTS_TABLE = "CREATE TABLE " + StudentEntry.TABLE_NAME + " ("
-                + StudentEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + StudentEntry.COLUMN_QR_CODE_ID + " TEXT NOT NULL, "
-                + StudentEntry.COLUMN_NAME + " TEXT NOT NULL, "
-                + StudentEntry.COLUMN_BATCH + " TEXT NOT NULL);";
-
-        db.execSQL(SQL_CREATE_STUDENTS_TABLE);
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + StudentEntry.TABLE_NAME);
+        db.execSQL(SQL_DELETE_ENTRIES);
         onCreate(db);
     }
 
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        super.onDowngrade(db, oldVersion, newVersion);
+    }
 
     public boolean create(String qr, String name, String batch) {
         SQLiteDatabase db = this.getWritableDatabase();
+
         ContentValues contentValues = new ContentValues();
         contentValues.put(StudentEntry.COLUMN_QR_CODE_ID, qr);
         contentValues.put(StudentEntry.COLUMN_NAME, name);
         contentValues.put(StudentEntry.COLUMN_BATCH, batch);
+
         long result = db.insert(StudentEntry.TABLE_NAME, null, contentValues);
         if (result == -1)
             return false;
@@ -49,8 +56,18 @@ public class StudentDbHelper extends SQLiteOpenHelper {
     }
 
     public Cursor read() {
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("select * from " + StudentEntry.TABLE_NAME, null);
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String[] projection = {StudentEntry._ID,
+                StudentEntry.COLUMN_QR_CODE_ID,
+                StudentEntry.COLUMN_NAME,
+                StudentEntry.COLUMN_BATCH};
+        Cursor cursor = db.query(StudentEntry.TABLE_NAME, projection,
+                null, null,
+                null, null, null);
+
+//        Cursor cursor = db.rawQuery("SELECT * FROM " + StudentEntry.TABLE_NAME, null);
+
         return cursor;
     }
 
