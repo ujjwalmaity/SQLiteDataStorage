ca5d29dc669e01ce517fc6e629cf796bfa33c98a
ujjwal <ujjwalmaity98@gmail.com>
2020-01-20 13:44:38 +0530

    Starter code for StudentProvider.java

---
+++ app/src/main/java/dev/ujjwal/sqlitedatastorage/data/StudentProvider.java
@@ -0,0 +1,66 @@
+package dev.ujjwal.sqlitedatastorage.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class StudentProvider extends ContentProvider {
+
+    /**
+     * Initialize the provider and the database helper object.
+     */
+    @Override
+    public boolean onCreate() {
+        // TODO: Create and initialize a PetDbHelper object to gain access to the pets database.
+        // Make sure the variable is a global variable, so it can be referenced from other
+        // ContentProvider methods.
+        return true;
+    }
+
+    /**
+     * Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
+     */
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    /**
+     * Insert new data into the provider with the given ContentValues.
+     */
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    /**
+     * Updates the data at the given selection and selection arguments, with the new ContentValues.
+     */
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * Delete the data at the given selection and selection arguments.
+     */
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * Returns the MIME type of data for the content URI.
+     */
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+}
